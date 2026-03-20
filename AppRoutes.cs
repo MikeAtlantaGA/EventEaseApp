2738a0b9364434a4ab14e6c9c5e03c9c7fe4d96a
MikeAtlantaGA <hockeypup@gmail.com>
2026-03-20 12:58:24 -0400

    Use base-relative app routes

--- AppRoutes.cs
+++ AppRoutes.cs
@@ -2,8 +2,8 @@ namespace EventEaseApp
 {
     public static class AppRoutes
     {
-        public const string Root = "/";
-        public const string Events = "/events";
-        public const string SessionTracker = "/session-tracker";
+        public const string Root = "";
+        public const string Events = "events";
+        public const string SessionTracker = "session-tracker";
     }
 }
