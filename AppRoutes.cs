18835937b770e61173150f62886779b68d902555
MikeAtlantaGA <hockeypup@gmail.com>
2026-03-20 12:14:29 -0400

    Deploy GitHub Pages site

--- AppRoutes.cs
+++
@@ -1,9 +0,0 @@
-namespace EventEaseApp
-{
-    public static class AppRoutes
-    {
-        public const string Root = "/";
-        public const string Events = "/events";
-        public const string SessionTracker = "/session-tracker";
-    }
-}
--- Models/SearchCriteria.cs
+++
@@ -1,9 +0,0 @@
-namespace EventEaseApp.Models
-{
-    public class SearchCriteria
-    {
-        public string Name { get; set; } = string.Empty;
-        public string Date { get; set; } = string.Empty;
-        public string Location { get; set; } = string.Empty;
-    }
-}
